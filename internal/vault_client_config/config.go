e76a818b3f0501a6ac5dc8e0e884eae50e2f1c4d
András Jáky <ajaky@cisco.com>
2023-07-31 16:07:35 +0200

    chore: copy code to create Vault client
    - copied from vault-secrets-webhook
    
    Signed-off-by: András Jáky <ajaky@cisco.com>

--- internal/vault_client_config/config.go
+++ internal/vault_client_config/config.go
@@ -16,14 +16,24 @@
 package vault_client_config
 
 import (
+	"context"
+	"crypto/x509"
+	"net/http"
 	"strconv"
 	"time"
 
+	"emperror.dev/errors"
+	"github.com/bank-vaults/vault-sdk/vault"
+	vaultapi "github.com/hashicorp/vault/api"
+	"github.com/sirupsen/logrus"
 	"github.com/slok/kubewebhook/v2/pkg/model"
 	"github.com/spf13/viper"
+	authenticationv1 "k8s.io/api/authentication/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	logrusadapter "logur.dev/adapter/logrus"
 )
 
 // VaultConfig represents vault options
@@ -501,4 +511,98 @@ func SetConfigDefaults() {
 	viper.SetDefault("VAULT_NAMESPACE", "")
 
 	viper.AutomaticEnv()
+}
+
+// NOTE: This was copied from vault-secrets-webhook/pkg/webhook/webhook.go
+func NewVaultClientFromVaultConfig(logger *logrus.Entry, k8sClient kubernetes.Interface, namespace string, vaultConfig VaultConfig) (*vault.Client, error) {
+	clientConfig := vaultapi.DefaultConfig()
+	if clientConfig.Error != nil {
+		return nil, clientConfig.Error
+	}
+
+	clientConfig.Address = vaultConfig.Addr
+
+	tlsConfig := vaultapi.TLSConfig{Insecure: vaultConfig.SkipVerify}
+	err := clientConfig.ConfigureTLS(&tlsConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	if vaultConfig.TLSSecret != "" {
+		tlsSecret, err := k8sClient.CoreV1().Secrets(namespace).Get(
+			context.Background(),
+			vaultConfig.TLSSecret,
+			metav1.GetOptions{},
+		)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to read Vault TLS Secret")
+		}
+
+		clientTLSConfig := clientConfig.HttpClient.Transport.(*http.Transport).TLSClientConfig
+
+		pool := x509.NewCertPool()
+
+		ok := pool.AppendCertsFromPEM(tlsSecret.Data["ca.crt"])
+		if !ok {
+			return nil, errors.Errorf("error loading Vault CA PEM from TLS Secret: %s", tlsSecret.Name)
+		}
+
+		clientTLSConfig.RootCAs = pool
+	}
+
+	if vaultConfig.VaultServiceAccount != "" {
+		sa, err := k8sClient.CoreV1().ServiceAccounts(vaultConfig.ObjectNamespace).Get(context.Background(), vaultConfig.VaultServiceAccount, metav1.GetOptions{})
+		if err != nil {
+			return nil, errors.Wrap(err, "Failed to retrieve specified service account on namespace "+vaultConfig.ObjectNamespace)
+		}
+
+		saToken := ""
+		if len(sa.Secrets) > 0 {
+			secret, err := k8sClient.CoreV1().Secrets(vaultConfig.ObjectNamespace).Get(context.Background(), sa.Secrets[0].Name, metav1.GetOptions{})
+			if err != nil {
+				return nil, errors.Wrap(err, "Failed to retrieve secret for service account "+sa.Secrets[0].Name+" in namespace "+vaultConfig.ObjectNamespace)
+			}
+			saToken = string(secret.Data["token"])
+		}
+
+		if saToken == "" {
+			tokenTTL := int64(600) // min allowed duration is 10 mins
+			tokenRequest := &authenticationv1.TokenRequest{
+				Spec: authenticationv1.TokenRequestSpec{
+					Audiences:         []string{"https://kubernetes.default.svc"},
+					ExpirationSeconds: &tokenTTL,
+				},
+			}
+
+			token, err := k8sClient.CoreV1().ServiceAccounts(vaultConfig.ObjectNamespace).CreateToken(
+				context.Background(),
+				vaultConfig.VaultServiceAccount,
+				tokenRequest,
+				metav1.CreateOptions{},
+			)
+			if err != nil {
+				return nil, errors.Wrap(err, "Failed to create a token for the specified service account "+vaultConfig.VaultServiceAccount+" on namespace "+vaultConfig.ObjectNamespace)
+			}
+			saToken = token.Status.Token
+		}
+
+		return vault.NewClientFromConfig(
+			clientConfig,
+			vault.ClientRole(vaultConfig.Role),
+			vault.ClientAuthPath(vaultConfig.Path),
+			vault.NamespacedSecretAuthMethod,
+			vault.ClientLogger(logrusadapter.NewFromEntry(logger)),
+			vault.ExistingSecret(saToken),
+			vault.VaultNamespace(vaultConfig.VaultNamespace),
+		)
+	}
+
+	return vault.NewClientFromConfig(
+		clientConfig,
+		vault.ClientRole(vaultConfig.Role),
+		vault.ClientAuthPath(vaultConfig.Path),
+		vault.ClientAuthMethod(vaultConfig.AuthMethod),
+		vault.ClientLogger(logrusadapter.NewFromEntry(logger)),
+		vault.VaultNamespace(vaultConfig.VaultNamespace),
+	)
 }
